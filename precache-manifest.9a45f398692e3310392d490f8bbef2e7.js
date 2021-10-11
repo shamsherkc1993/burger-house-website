fd75d49b953d6082ef446ba30650706bff24f6e2
shamsherkc1993 <shamsher.kc.c4@gmail.com>
2021-10-12 00:54:08 +0900

    Updates

---
+++ precache-manifest.9a45f398692e3310392d490f8bbef2e7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83f203fbedb43f735a47235eb4b3fec",
+    "url": "/burger-house-website/index.html"
+  },
+  {
+    "revision": "17fbfad0cd30191a314a",
+    "url": "/burger-house-website/static/css/2.e4d55abd.chunk.css"
+  },
+  {
+    "revision": "efd0da1293c2c0920d0b",
+    "url": "/burger-house-website/static/css/main.4950f548.chunk.css"
+  },
+  {
+    "revision": "17fbfad0cd30191a314a",
+    "url": "/burger-house-website/static/js/2.80c81605.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-house-website/static/js/2.80c81605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd0da1293c2c0920d0b",
+    "url": "/burger-house-website/static/js/main.214cfdc9.chunk.js"
+  },
+  {
+    "revision": "5ad238d194b28d30fccf",
+    "url": "/burger-house-website/static/js/runtime-main.7c66ff89.js"
+  },
+  {
+    "revision": "3601dd6585bf3e4db2027a40370f26bd",
+    "url": "/burger-house-website/static/media/abt.3601dd65.jpg"
+  },
+  {
+    "revision": "b60f8aba911f0ba054cf0a5decd79830",
+    "url": "/burger-house-website/static/media/burger.b60f8aba.jpg"
+  },
+  {
+    "revision": "ad5fa503c9651ff10346c937c84c0802",
+    "url": "/burger-house-website/static/media/burger1.ad5fa503.jpg"
+  },
+  {
+    "revision": "9beee3a5f3956f68dde7a82e1591a5b0",
+    "url": "/burger-house-website/static/media/burger2.9beee3a5.jpg"
+  },
+  {
+    "revision": "e4ab64045f2c9e98793b8907b834fa19",
+    "url": "/burger-house-website/static/media/burger3.e4ab6404.jpg"
+  },
+  {
+    "revision": "bfe17a5f33d8e8a8efed81b60f75dbb8",
+    "url": "/burger-house-website/static/media/burger4.bfe17a5f.jpg"
+  },
+  {
+    "revision": "8932b4dec2e443d5ebdb071fdf825ccd",
+    "url": "/burger-house-website/static/media/burger5.8932b4de.jpg"
+  },
+  {
+    "revision": "d059d40e5c9a5c1ff4fb1d3b78e592c3",
+    "url": "/burger-house-website/static/media/burger6.d059d40e.jpg"
+  }
+]);
